67317541a2bcdfb4304ffa52c2fce8585f380f78
Curious-Firdosh <thefirdosh@gmail.com>
2025-04-04 14:49:04 +0530

    Lerned How Math And Number works

---
+++ JavaScript-Interview/01_Basics/06_Nums&Maths.js
@@ -0,0 +1,50 @@
+//++++++++++++++++++++++++++++++++ NUmber +++++++++++++++++++++++++++++
+
+// const score = 1000
+//! console.log(typeof score); //number
+//! console.log(score);// ook
+
+const Score1 = new Number(100) 
+
+// console.log(typeof Score1);//object
+// console.log(score);
+// console.log(score.toFixed(3));//.000
+
+const score2 = 3400000000
+// console.log(score2);//same  but 
+// console.log(score2.toLocaleString("en-In")); //3,40,00,00,000 Indian Standeras 
+
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MIN_SAFE_INTEGER);
+
+
+//++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++++
+
+// *console.log(Math.abs(-4)); // Only Work FO rNegetive TO pOSTIVE 
+// *console.log(Math.round(890.098));
+// *console.log(Math.round(90.49999));
+// *console.log(Math.min([1,5,6,456,9])); //NaN oUTPUT Hey Gpt Explian HTat WHy That Happend
+// *console.log(Math.max(1,5,6,456,9));
+
+//@console.log(Math.random()); // Only Vlaue Come Betwen 1 and 2
+//@ console.log( Math.floor(Math.random() *10) + 1); // Only COme Betwenn 1 to 10 
+
+const min = 10 
+const max = 20 
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + 10 ); // that return alwaysfrom 10 to 20 value 
+
+
+
+
+
+
+
+
+
+
+
+
+
