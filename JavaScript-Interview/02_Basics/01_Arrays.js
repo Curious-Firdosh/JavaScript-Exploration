efb0ed819b675fc066d1e7203eb3e3c7af6a8398
Curious-Firdosh <thefirdosh@gmail.com>
2025-04-11 21:09:54 +0530

    Learning Array

---
+++ JavaScript-Interview/02_Basics/01_Arrays.js
@@ -0,0 +1,10 @@
+    const heroes = [1122, "Hello" , null , undefined , {name : "Md Firdosh"}, "ShaktiMaan"]
+    console.log( heroes[4].name);
+
+    const myarr2 = new Array("kala" , 456 , "kiHaalChhaaal")
+    console.log(myarr2);
+
+// Array Methods 
+
+    myarr2.push("ShaktiMaaaan")
+    myarr2.pop()
