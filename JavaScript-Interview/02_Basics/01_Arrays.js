41b6c3a6f1de08ffa398da431f017093cde408f8
Curious-Firdosh <thefirdosh@gmail.com>
2025-04-11 21:24:14 +0530

    Array

--- JavaScript-Interview/02_Basics/01_Arrays.js
+++ JavaScript-Interview/02_Basics/01_Arrays.js
@@ -1,10 +1,24 @@
-    const heroes = [1122, "Hello" , null , undefined , {name : "Md Firdosh"}, "ShaktiMaan"]
-    console.log( heroes[4].name);
+    // const heroes = [1122, "Hello" , null , undefined , {name : "Md Firdosh"}, "ShaktiMaan"]
+    // console.log( heroes[4].name);
 
     const myarr2 = new Array("kala" , 456 , "kiHaalChhaaal")
     console.log(myarr2);
 
 // Array Methods 
 
-    myarr2.push("ShaktiMaaaan")
-    myarr2.pop()
+    // myarr2.push("ShaktiMaaaan")
+    // myarr2.pop() // remove last element from array 
+
+    // myarr2.shift() // remove 1st elemnt from array 
+    // myarr2.unshift("890@@") // addd elemt to shift item from 1st index 
+    // console.log(myarr2.includes("kala")); // true
+    // console.log(myarr2.indexOf(98)); //  if that value not exist that give us -1 if exist then give answer
+    // console.log(myarr2.indexOf(456));
+
+    const newArray = myarr2.join()
+    console.log(newArray); // but due to jio it is converted into String    
+    
+    
+    
+
+
